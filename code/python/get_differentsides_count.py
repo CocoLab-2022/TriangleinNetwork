cf1426d2a9a5b320c61e42dcc9f90287c7a3a145
19991010wan <2996808587@qq.com>
2022-07-27 23:06:53 +0800

    update differentsides_count

--- code/python/get_differentsides_count.py
+++ code/python/get_differentsides_count.py
@@ -5,7 +5,7 @@ import networkx as nx
 
 
 
-from python.network_extraction_from_issue_comment import get_ower_repo_list, newoutputfile
+from network_extraction_from_issue_comment import get_ower_repo_list, newoutputfile
 
 #Author: Zhijie Wan
 
@@ -65,7 +65,7 @@ def get_triangle_count():
         with open(outpufile, 'a+', encoding='utf-8') as f:
             f.write("%s\n" % count)
 
-# Get the number of quads via networkx's enumerate_all_cliques
+# Get the number of specific sides via networkx's enumerate_all_cliques
 def enumerate_all_cliques_size_k(G, k):
     i = 0
     for clique in nx.enumerate_all_cliques(G):
@@ -88,7 +88,47 @@ def get_quadrangle_count():
         with open(outpufile, 'a+', encoding='utf-8') as f:
             f.write("%s\n" % count)
 
+def get_pentagon_count():
+    repos = get_ower_repo_list("repos")
+    # print(owers)
+    # print(repos)
+    outputfilename = "pentagon_count"
+    outpufile = newoutputfile(outputfilename, "count")
+    # print(nx.triangles(build_graph_by_networkx("homebrew-cask")))
+    for i in range(0, 200):
+        count = {repos[i]: enumerate_all_cliques_size_k(build_graph_by_networkx(repos[i]), 5)}
+        print(count)
+        with open(outpufile, 'a+', encoding='utf-8') as f:
+            f.write("%s\n" % count)
+
+def get_star_count(star):
+    repos = get_ower_repo_list("repos")
+    # print(owers)
+    # print(repos)
+    outputfilename = "stars>" + str(star) + "_count"
+    outpufile = newoutputfile(outputfilename, "count")
+    for i in range(0, 200):
+        nodes_degree = []
+        path_3 = "../data/nodesdegree/" + repos[i] + "_nodes_degree"
+        with open(path_3, 'r+', encoding='utf-8') as f:
+            for line in f:
+                nodes_degree_list = ast.literal_eval(line)
+                nodes_degree.append(nodes_degree_list)
+
+        starcount = 0
+        lenth = len(nodes_degree)
+        for j in range(lenth):
+            if nodes_degree[j][1] >= star:
+                starcount += 1
+        # print(repos[i] + "星型图个数为" + starcount)
+        star_count = {repos[i]: starcount}
+        with open(outpufile, 'a+', encoding='utf-8') as f:
+            f.write("%s\n" % star_count)
+
 if __name__ == "__main__":
     # get_triangle_count()
-    get_quadrangle_count()
-
+    # get_quadrangle_count()
+    # get_pentagon_count()
+    get_star_count(3)
+    get_star_count(4)
+    get_star_count(5)
